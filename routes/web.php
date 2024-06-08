9c5f5b21f14604d1b00e26f5b64f69e73389a23c
Mohammed Najib Shaaban <76607364+Shaabanm2018@users.noreply.github.com>
2024-06-08 13:08:47 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\ReviewController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
+Route::get('/', function () {
+    return redirect()->route('books.index');
+});
+
+Route::resource('books',BookController::class)->only(['index','show']);
+Route::resource('books.reviews',ReviewController::class)->scoped(['review'=> 'book'])->only(['create','store']);
